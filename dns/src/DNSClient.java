ca389001ed6c7d3f1100bb458a3dae04fc45a370
Guanming Lin <guanming.lin@stonybrook.edu>
2015-12-03 04:42:57 -0500

    done with single name server

--- dns/src/DNSClient.java
+++ dns/src/DNSClient.java
@@ -1,4 +1,5 @@
 import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -9,19 +10,20 @@ import java.net.Socket;
 public class DNSClient {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		String clientCommand;
-		String serverResponse;
-		
+		byte[] serverResponse;
+		int responseLength;
+
 		BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
 		Socket clientSocket = new Socket(args[0], Integer.parseInt(args[1]));
 		clientSocket.setSoTimeout(10000);
 		DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
-		BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+		DataInputStream inFromServer = new DataInputStream(clientSocket.getInputStream());
 		while (!clientSocket.isClosed()) {
-			
+
 			clientCommand = inFromUser.readLine();
-			
+
 			outToServer.writeBytes(clientCommand + "\n");
 			if (clientCommand.equals("exit")) {
 				inFromUser.close();
@@ -31,25 +33,27 @@ public class DNSClient {
 				printHelp();
 			}
 			else {
-				serverResponse = inFromServer.readLine();
-				System.out.println(serverResponse);
-			}	
-			
-		}
+				responseLength = inFromServer.readInt();
+				serverResponse = new byte[responseLength];
+				inFromServer.readFully(serverResponse);
+				System.out.println(new String(serverResponse));
+			}
+		}	
+
 	}
 
 	private static void printHelp() {
 		String helpMessage;
 		helpMessage = "\nhelp: prints a list of supported commands\n"
-		+ "\nput [name] [value] [type]: adds name record to the database "
-		+ "or updates the record with the new value\n"
-		+ "\nget [name] [type]: returns the value of the record with the "
-		+ "provided name and type\n"
-		+ "\ndel [name] [type]: removes the record with the provided "
-		+ "name and type from the database\n"
-		+ "\nbrowse: displays the name and type of all current name records "
-		+ "in the database\n"
-		+ "\nexit: terminates connection with the server and exits the program\n";
+				+ "\nput [name] [value] [type]: adds name record to the database "
+				+ "or updates the record with the new value\n"
+				+ "\nget [name] [type]: returns the value of the record with the "
+				+ "provided name and type\n"
+				+ "\ndel [name] [type]: removes the record with the provided "
+				+ "name and type from the database\n"
+				+ "\nbrowse: displays the name and type of all current name records "
+				+ "in the database\n"
+				+ "\nexit: terminates connection with the server and exits the program\n";
 		System.out.println(helpMessage);
 	}
 }
--- dns/src/DNSServer.java
+++ dns/src/DNSServer.java
@@ -25,7 +25,7 @@ public class DNSServer {
 
 		public void run() {
 			String clientCommand;
-			String[] commandArgs = new String[4];
+			String[] commandArgs;
 			String serverResponse = "";
 
 			System.out.println("A connection has been made.");
@@ -35,28 +35,36 @@ public class DNSServer {
 				while (!connectionSocket.isClosed()) {
 					clientCommand = inFromClient.readLine();
 					commandArgs = clientCommand.split(" ");
-					switch (commandArgs[0]) {
-					case "put":
-						serverResponse = put(commandArgs[1], commandArgs[2], commandArgs[3]);
-						break;
-					case "get":
-						serverResponse = get(commandArgs[1], commandArgs[2]);
-						break;
-					case "del":
-						serverResponse = del(commandArgs[1], commandArgs[2]);
-						break;
-					case "browse":
-						serverResponse = browse();
-						break;
-					case "exit":
-						connectionSocket.close();
-						break;
-					default:
-						break;
+					try {
+						switch (commandArgs[0]) {
+						case "put":
+							serverResponse = put(commandArgs[1], commandArgs[2], commandArgs[3]);
+							break;
+						case "get":
+							serverResponse = get(commandArgs[1], commandArgs[2]);
+							break;
+						case "del":
+							serverResponse = del(commandArgs[1], commandArgs[2]);
+							break;
+						case "browse":
+							serverResponse = browse();
+							break;
+						case "exit":
+							connectionSocket.close();
+							break;
+						default:
+							serverResponse = "Incorrect command.";
+							break;
+						}
+					}
+					catch (ArrayIndexOutOfBoundsException e) {
+						serverResponse = "Missing arguments.";
 					}
 					if (!clientCommand.equals("exit")) {
-						outToClient.writeBytes(serverResponse + "\n");
+						outToClient.writeInt(serverResponse.length());
+						outToClient.writeBytes(serverResponse);
 					}
+
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -71,52 +79,52 @@ public class DNSServer {
 	@SuppressWarnings("unchecked")
 	public static void main(String[] args) throws IOException {
 
+		try {
+			File f = new File("database");
+			if (f.exists()) {
+				FileInputStream fis = new FileInputStream(f);
+				ObjectInputStream ois = new ObjectInputStream(fis);
+				database = (TreeMap<String, TreeMap<String, String>>) ois.readObject();
+				ois.close();
+				fis.close();
+			}
+			else {
+				database = new TreeMap<String, TreeMap<String, String>>();
+			}
+			final ServerSocket serverSocket = new ServerSocket(0);
+
+			System.out.println("Server has been started on port " + serverSocket.getLocalPort() + ".");
+
+			Runtime.getRuntime().addShutdownHook(new Thread() {
+				public void run() {
+					try {
+						serverSocket.close();
+						System.out.println("Server has been stopped.");
+						FileOutputStream fos = new FileOutputStream("database");
+						ObjectOutputStream oos = new ObjectOutputStream(fos);
+						oos.writeObject(database);
+						oos.close();
+						fos.close();
+					} catch (IOException e) {
+						e.printStackTrace();
+					}
+				}
+			});
+
+			while(true) {
 				try {
-					File f = new File("database");
-					if (f.exists()) {
-						FileInputStream fis = new FileInputStream(f);
-						ObjectInputStream ois = new ObjectInputStream(fis);
-						database = (TreeMap<String, TreeMap<String, String>>) ois.readObject();
-						ois.close();
-						fis.close();
-					}
-					else {
-						database = new TreeMap<String, TreeMap<String, String>>();
-					}
-					final ServerSocket serverSocket = new ServerSocket(0);
-
-					System.out.println("Server has been started on port " + serverSocket.getLocalPort() + ".");
-
-					Runtime.getRuntime().addShutdownHook(new Thread() {
-						public void run() {
-							try {
-								serverSocket.close();
-								System.out.println("Server has been stopped.");
-								FileOutputStream fos = new FileOutputStream("database");
-								ObjectOutputStream oos = new ObjectOutputStream(fos);
-								oos.writeObject(database);
-								oos.close();
-								fos.close();
-							} catch (IOException e) {
-								e.printStackTrace();
-							}
-						}
-					});
-					
-					while(true) {
-						try {
-							Socket connectionSocket = serverSocket.accept();
-							new Thread(new ClientRequest(connectionSocket)).start();
-						}
-						catch (SocketException e) {
-						}
-					}
-				} catch (IOException e) {
-					e.printStackTrace();
-				} catch (ClassNotFoundException e) {
-					e.printStackTrace();
+					Socket connectionSocket = serverSocket.accept();
+					new Thread(new ClientRequest(connectionSocket)).start();
+				}
+				catch (SocketException e) {
 				}
 			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public static String put(String name, String value, String type) {
 		synchronized(database) {
@@ -133,43 +141,50 @@ public class DNSServer {
 			}
 		}
 	}
-	
+
 	public static String get(String name, String type) {
 		synchronized(database) {
 			if (!database.containsKey(type) || !database.get(type).containsKey(name)) {
-				return "The record of type " + type + 
+				return "404 Not Found\n"
+						+ "The record of type " + type + 
 						" and name " + name + " cannot be found.";
 			}
 			else {
-				return database.get(type).get(name);
+				return "200 OK\n" + database.get(type).get(name);
 			}
 		}
 	}
-	
+
 	public static String del(String name, String type) {
 		synchronized(database) {
 			if (!database.containsKey(type) || !database.get(type).containsKey(name)) {
-				return "The record of type " + type + 
+				return "404 Not Found\n"
+						+ "The record of type " + type + 
 						" and name " + name + " cannot be found.";
 			}
 			else {
 				database.get(type).remove(name);
-				return "The record has been removed.";
+				return "200 OK\nThe record has been removed.";
 			}
 		}
 	}
-	
+
 	public static String browse() {
 		synchronized(database) {
 			String records = "";
 			for (String type : database.keySet()) {
 				for (String name : database.get(type).keySet()) {
-					records += "Name: " + name + "\n"
+					records += "\nName: " + name + "\n"
 							+ "Type: " + type + "\n";
 				}
 			}
-			return records;
+			if (records.equals("")) {
+				return "200 OK\nThe database is empty.";
+			}
+			else {
+				return "200 OK\n" + records;
+			}
 		}
 	}
-	
+
 }
